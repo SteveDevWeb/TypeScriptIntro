4c7cb44a958c7259d459d097c713b2d832b7c8a8
Steve <stevele99@yahoo.fr>
2023-10-13 17:31:26 +0200

    ESlint Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <h1>Hello</h1>
-  );
+      const d = 'Max'
+      return <h1>Hsello {d}</h1>
 }
 
-export default App;
+export default App
